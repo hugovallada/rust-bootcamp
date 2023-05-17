816c340300f055bd4b5f404f03efac7185b8013d
Hugo Vallada <valladahugo@gmail.com>
2023-05-17 13:47:31 -0300

    Implementation block

--- Beginner/build_your_own_datatypes/structs/src/main.rs
+++ Beginner/build_your_own_datatypes/structs/src/main.rs
@@ -1,20 +1,48 @@
 struct Product {
     name: String,
     price: f32,
-    in_stock: bool
+    in_stock: bool,
+}
+
+impl Product {
+    fn calculate_sales_tax_rate(&self) -> f32 {
+        self.price * 0.1
+    }
+
+    fn set_price(&mut self, price: f32) {
+        self.price = price;
+    }
+
+    fn buy(self) -> i32 {
+        let name = self.name;
+        println!("{name} was bought");
+        123
+    }
+
+    // Associate function - Chamadas de static methods em outras linguagens
+    fn get_default_sales_tax() -> f32 {
+        0.1
+    }
+
+    fn new(name: String, price: f32) -> Product {
+        Product { name: name, price: price, in_stock: true }
+    }
 }
 
 fn main() {
     let mut book = Product {
         name: String::from("Book"),
-                price: 100.0,
-                in_stock: true
+        price: 100.0,
+        in_stock: true,
     };
     book.in_stock = false;
-    let sales_tax = calculate_sales_tax(&book);
-    println!("Sales: {}", sales_tax)
-}
+    let sales_tax = book.calculate_sales_tax_rate();
+    println!("Sales: {}", sales_tax);
+    book.set_price(1.0);
+    book.buy();
 
-fn calculate_sales_tax(product: &Product) -> f32 {
-    product.price * 0.1
+    let default_tax = Product::get_default_sales_tax();
+    println!("Default tax: {default_tax}");
+
+    let car = Product::new(String::from("Carro"), 20000.00);
 }
